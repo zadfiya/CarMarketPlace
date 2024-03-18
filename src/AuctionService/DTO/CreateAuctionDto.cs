e8ca07188c63c798d47618970283a3b9e992c65e
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-17 21:43:36 -0400

    Made every field required

--- src/AuctionService/DTO/CreateAuctionDto.cs
+++ src/AuctionService/DTO/CreateAuctionDto.cs
@@ -1,13 +1,23 @@
-﻿namespace AuctionService;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionService;
 
 public class CreateAuctionDto
 {
+    [Required]
     public string Make { get; set; }
+    [Required]
     public string Model {get; set;}
+    [Required]
     public int Year { get; set; }
+    [Required]
     public string Color { get; set; }
+    [Required]
     public double Mileage { get; set; }
+    [Required]
     public string ImageUrl {get;set;}
+    [Required]
     public int ReservedPrice { get; set; } = 0;
+    [Required]
     public DateTime AuctionEndDate {get; set;} = DateTime.Now;
 }
---
+++ src/AuctionService/DTO/UpdateAuctionDto.cs
@@ -0,0 +1,13 @@
+﻿namespace AuctionService;
+
+public class UpdateAuctionDto
+{
+    public string? Make { get; set; }
+    public string? Model {get; set;}
+    public int? Year { get; set; }
+    public string? Color { get; set; }
+    public double? Mileage { get; set; }
+    public string? ImageUrl {get;set;}
+    public int? ReservedPrice { get; set; } = 0;
+    public DateTime? AuctionEndDate {get; set;} = DateTime.Now;
+}
