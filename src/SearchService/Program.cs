61a02222a2ca96bf24e358989510d16d796a6feb
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-18 21:03:54 -0400

    Item Model Added

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,7 +1,23 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
+
+await DB.InitAsync("searchDb", MongoClientSettings
+                                .FromConnectionString(
+                                    builder.Configuration.GetConnectionString("MongoDbClient")));
+
+await DB.Index<Item>()
+    .Key(x=>x.Make, KeyType.Text)
+    .Key(x=>x.Model, KeyType.Text)
+    .Key(x=>x.Color, KeyType.Text)
+    .CreateAsync();
+
+
 
 app.Run();
 
