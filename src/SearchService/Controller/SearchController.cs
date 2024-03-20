04561dfe22868f72ad7f9f8e49a8bef2f5116c01
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-19 23:05:44 -0400

    Search Service Ocupied with Pagination

--- src/SearchService/Controller/SearchController.cs
+++ src/SearchService/Controller/SearchController.cs
@@ -13,13 +13,26 @@ public class SearchController :ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SerachItems()
+    public async Task<ActionResult<List<Item>>> SerachItems([FromQuery]string searchTerm,
+                                                            [FromQuery]int pageNumber=1,
+                                                            [FromQuery]int pageSize=5)
     {
-        var query = DB.Find<Item>();
+        var query = DB.PagedSearch<Item>();
         query.Sort(x=>x.Ascending(a=>a.Make));
+
+        if(!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full,searchTerm).SortByTextScore();
+        }
+
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
        
        var result = await query.ExecuteAsync();
-        Console.WriteLine(result);
-        return result;
+        return Ok(new{
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 }
