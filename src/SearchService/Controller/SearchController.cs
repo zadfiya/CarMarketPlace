0295829a53f656f8f1f4b66e995efd4d8aa1c4de
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-19 22:55:39 -0400

    Search Service Get Endpoint

---
+++ src/SearchService/Controller/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SearchController :ControllerBase
+{
+    public SearchController()
+    {
+        
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SerachItems()
+    {
+        var query = DB.Find<Item>();
+        query.Sort(x=>x.Ascending(a=>a.Make));
+       
+       var result = await query.ExecuteAsync();
+        Console.WriteLine(result);
+        return result;
+    }
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -2,10 +2,11 @@ using SearchService;
 using SearchService.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddControllers();
 
 var app = builder.Build();
 await DbInitializer.InitDb(app);
+app.MapControllers();
 
 app.Run();
 
