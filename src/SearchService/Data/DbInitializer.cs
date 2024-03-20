20856d990761c81fec06dcbb5f9cbb751f10b496
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-19 22:19:48 -0400

    Seeding MongoDB

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+namespace SearchService.DbInitializer;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("searchDb", MongoClientSettings
+                                .FromConnectionString(
+                                    app.Configuration.GetConnectionString("MongoDbClient")));
+
+        await DB.Index<Item>()
+            .Key(x=>x.Make, KeyType.Text)
+            .Key(x=>x.Model, KeyType.Text)
+            .Key(x=>x.Color, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+        if(count==0)
+        {
+            Console.WriteLine("Seeding Data");
+            await SeedData();
+            return ;
+        }
+    }
+
+    public static async Task SeedData()
+    {
+        var data = await File.ReadAllTextAsync("Data/Auction.json");
+        var options = new JsonSerializerOptions{PropertyNameCaseInsensitive=true};
+        var auctions = JsonSerializer.Deserialize<List<Item>>(data,options);
+
+        await DB.SaveAsync<Item>(auctions);
+    }
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,23 +1,11 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
 using SearchService;
+using SearchService.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
-
-await DB.InitAsync("searchDb", MongoClientSettings
-                                .FromConnectionString(
-                                    builder.Configuration.GetConnectionString("MongoDbClient")));
-
-await DB.Index<Item>()
-    .Key(x=>x.Make, KeyType.Text)
-    .Key(x=>x.Model, KeyType.Text)
-    .Key(x=>x.Color, KeyType.Text)
-    .CreateAsync();
-
-
+await DbInitializer.InitDb(app);
 
 app.Run();
 
