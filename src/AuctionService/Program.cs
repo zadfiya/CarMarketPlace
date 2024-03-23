dba9bfe5f18ea647f51e9e3089a9a53c2b7c036d
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-23 18:00:01 -0400

    Endpoint Configured of Mass Transit for autionService

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,6 +1,6 @@
 using AuctionService;
 using Microsoft.EntityFrameworkCore;
-
+using MassTransit;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -8,7 +8,15 @@ builder.Services.AddDbContext<AuctionDBContext>(option=>{
     option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
+builder.Services.AddMassTransit(x=>{
+    x.UsingRabbitMq( (context,cfg) =>{
+            //Configure the endpoints for all defined consumer, saga, and activity types using an optional endpoint name formatter. 
+            //If no endpoint name formatter is specified and an IEndpointNameFormatter is registered in the container, it is resolved from the container. 
+            //Otherwise, the DefaultEndpointNameFormatter is used.
+            cfg.ConfigureEndpoints(context);
+        }
+    );
+});
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
