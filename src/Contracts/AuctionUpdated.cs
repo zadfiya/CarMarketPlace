9a9dabd87e072e088e1b325051675dc5aa76b698
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-23 18:13:15 -0400

    AuctionUpdated Event Added

---
+++ src/Contracts/AuctionUpdated.cs
@@ -0,0 +1,13 @@
+﻿namespace Contracts;
+
+public class AuctionUpdated
+{
+    public string Make { get; set; }
+    public string Model {get; set;}
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public double Mileage { get; set; }
+    public string ImageUrl {get;set;}
+    public int ReservedPrice { get; set; } = 0;
+    public DateTime AuctionEndDate {get; set;} = DateTime.Now;
+}
