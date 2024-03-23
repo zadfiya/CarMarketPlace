f89793ec2c222990f88a0f06beafbb4e2a0173ad
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-23 18:00:27 -0400

    Endpoint Configured of Mass Transit for SearchService

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,8 +1,18 @@
+using MassTransit;
 using SearchService;
 using SearchService.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddMassTransit(x=>{
+    x.UsingRabbitMq( (context,cfg) =>{
+            //Configure the endpoints for all defined consumer, saga, and activity types using an optional endpoint name formatter. 
+            //If no endpoint name formatter is specified and an IEndpointNameFormatter is registered in the container, it is resolved from the container. 
+            //Otherwise, the DefaultEndpointNameFormatter is used.
+            cfg.ConfigureEndpoints(context);
+        }
+    );
+});
 
 var app = builder.Build();
 await DbInitializer.InitDb(app);
