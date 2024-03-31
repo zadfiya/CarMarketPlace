1f3e4cf79a8292bdb3ddafc8689ef61375600053
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-31 18:32:47 -0400

    Delete Auction is synced with search service

---
+++ src/SearchService/Consumers/AuctionDeleteConsumer.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionDeleteConsumer : IConsumer<AuctionDeleted>
+{
+    public async Task Consume(ConsumeContext<AuctionDeleted> context)
+    {
+        var result = await DB.DeleteAsync<Item>(context.Message.Id);
+
+        if(!result.IsAcknowledged)
+           throw new MessageException(typeof(AuctionDeleted),"Unable to Delete MongoDB Item with the id: "+ context.Message.Id);
+    }
+}
