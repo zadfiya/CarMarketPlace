1759776ced4ff3065eee011ece78969cd74fc1fd
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-31 17:26:55 -0400

    Created Falty Consumer that cath exception in search service

---
+++ src/AuctionService/Consumer/AuctionCreatedFaultyConsumer.cs
@@ -0,0 +1,14 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class AuctionCreatedFaultyConsumer : IConsumer<Fault<AuctionCreated>>
+{
+    public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+    {
+        Console.WriteLine("--> Faulty Consumer Recieved from Search Service Fault id: "+ context.Message.FaultId + " and Aution id:" + context.Message.Message.Id);
+
+        await context.Publish(context.Message.Message);
+    }
+}
