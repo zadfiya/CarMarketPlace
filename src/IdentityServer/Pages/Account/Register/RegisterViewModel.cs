9c4f7a55cca1e2d7d16e487a33df1702c69f7632
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-04-09 19:36:32 -0400

    ViewModel Added for register Class

---
+++ src/IdentityServer/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServer;
+
+public class RegisterViewModel
+{
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string FullName { get; set; }
+    [Required]
+    public int Button { get; set; }
+    [Required]
+    public int ReturnUrl { get; set; }
+
+}
