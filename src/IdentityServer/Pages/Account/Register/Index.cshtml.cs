e9287ecb39f581be1a1171d2de8adcda7eb11987
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-04-09 19:33:18 -0400

    Reference to Register Page added

--- src/IdentityServer/Pages/Account/Register/Index.cshtml.cs
+++ src/IdentityServer/Pages/Account/Register/Index.cshtml.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IdentityServer.Pages.Register
 {
+    [SecurityHeaders]
+    [AllowAnonymous]
     public class IndexModel : PageModel
     {
         public void OnGet()
