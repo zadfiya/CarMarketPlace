7778638f5d62d8e785e5a2a7295c66bc712ef4ba
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-08-23 19:43:41 -0400

    YARP Setup

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+app.MapReverseProxy();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
